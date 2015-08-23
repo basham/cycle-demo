fad766d01cca396573ac350c593f0930c1facadc
Chris Basham <chris@bash.am>
2015-08-23 00:41:27 -0400

    Abstract url generation

--- src/main.js
+++ src/main.js
@@ -10,31 +10,36 @@ function intent(Router) {
   };
 }
 
-function model(actions) {
+function model(actions, Router) {
   return actions.transition$.map(({toState, fromState}) => {
     // Since Router5 doesn't allow arbitrary data to be forwarded through it,
     // find the original route object and initiate its handler.
     let route = routes.filter(({name}) => name === toState.name)[0];
     let handler = route.handler();
     let name = toState.name;
-    return {name, handler};
+    let homeUrl = Router.buildUrl('home');
+    let helloUrl = Router.buildUrl('hello');
+    return {name, handler, homeUrl, helloUrl};
   });
 }
 
 function view(state$) {
-  return state$.map(({name, handler}) =>
+  return state$.map(({name, handler, homeUrl, helloUrl}) =>
     <div>
       <p>Hi, {name}</p>
-      <a href="#/">Home</a>
-      <a href="#/hello">Hello</a>
+      <a href={homeUrl}>Home</a>
+      <a href={helloUrl}>Hello</a>
       {handler}
     </div>
   );
 }
 
 export default function main({Router}) {
+  let actions = intent(Router);
+  let state$ = model(actions, Router);
+  let view$ = view(state$);
   return {
-    DOM: view(model(intent(Router))),
+    DOM: view$,
     Router: Rx.Observable.from(['start'])
   }
 }
