869b12831a4194d1b9cfc2f3bf3d31fcfa3a1778
Chris Basham <chris@bash.am>
2015-08-23 01:22:31 -0400

    Rearrange buildUrl

--- src/main.js
+++ src/main.js
@@ -10,25 +10,23 @@ function intent(Router) {
   };
 }
 
-function model(actions, Router) {
+function model(actions) {
   return actions.transition$.map(({toState, fromState}) => {
     // Since Router5 doesn't allow arbitrary data to be forwarded through it,
     // find the original route object and initiate its handler.
     let route = routes.filter(({name}) => name === toState.name)[0];
     let handler = route.handler();
     let name = toState.name;
-    let homeUrl = Router.buildUrl('home');
-    let helloUrl = Router.buildUrl('hello');
-    return {name, handler, homeUrl, helloUrl};
+    return {name, handler};
   });
 }
 
-function view(state$) {
-  return state$.map(({name, handler, homeUrl, helloUrl}) =>
+function view(state$, Router) {
+  return state$.map(({name, handler}) =>
     <div>
       <p>Hi, {name}</p>
-      <a href={homeUrl}>Home</a>
-      <a href={helloUrl}>Hello</a>
+      <a href={Router.buildUrl('home')}>Home</a>
+      <a href={Router.buildUrl('hello')}>Hello</a>
       {handler}
     </div>
   );
@@ -36,8 +34,8 @@ function view(state$) {
 
 export default function main({Router}) {
   let actions = intent(Router);
-  let state$ = model(actions, Router);
-  let view$ = view(state$);
+  let state$ = model(actions);
+  let view$ = view(state$, Router);
   return {
     DOM: view$,
     Router: Rx.Observable.from(['start'])
