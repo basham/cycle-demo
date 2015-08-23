5efe6be081d16154c0ef46b2dd2f6f7954ac1f74
Chris Basham <chris@bash.am>
2015-08-22 21:11:35 -0400

    Integrate director router

---
+++ src/Search.js
@@ -0,0 +1,45 @@
+/** @jsx hJSX */
+import {Rx} from '@cycle/core';
+import {hJSX} from '@cycle/dom';
+import classNames from 'classnames';
+
+import formStyles from './Form.less';
+import headerStyles from './Header.less';
+
+function intent(DOM) {
+  return {
+    changeName$: DOM.get('.js-name-input', 'input')
+      .map(ev => ev.target.value)
+  };
+}
+
+function model(actions) {
+  return Rx.Observable.combineLatest(
+    actions.changeName$.startWith(''),
+    (name) => ({name})
+  );
+}
+
+function view(state$) {
+  return state$.map(({name}) =>
+    <div>
+      <div className={formStyles.block}>
+        <label className={formStyles.label}>
+          Name
+        </label>
+        <input
+          className={classNames(formStyles.textInput, 'js-name-input')}
+          type="text"/>
+      </div>
+      <h1 className={headerStyles.h1}>Hello {name}</h1>
+    </div>
+  );
+}
+
+export default function Search({DOM}) {
+  let actions = intent(DOM);
+  let state$ = model(actions);
+  return {
+    DOM: view(state$)
+  };
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,54 @@
-import {run} from '@cycle/core';
-import {makeDOMDriver} from '@cycle/dom';
-import main from './main';
+/** @jsx hJSX */
+import {run, Rx} from '@cycle/core';
+import {makeDOMDriver, h, hJSX} from '@cycle/dom';
+import makeRouterDriver from 'cycle-director';
+
+import Search from './Search';
 
 if(module.hot) {
   module.hot.accept();
 }
 
+let routes = [
+  {
+    url: '/',
+    on: () => { return h('search') },
+    name: 'Home'
+  },
+  {
+    url: '/hello',
+    on: () => { return h('h2', 'another route!') },
+    name: 'HELLO, YOU!'
+  }
+];
+
+function view(output) {
+  return (
+    <div>
+      <p>Oh, hi</p>
+      <a href="#/">Home</a>
+      <a href="#/hello">Hello</a>
+      {output}
+    </div>
+  );
+}
+
+function main({DOM, Router}) {
+  let route$ = Rx.Observable.from(routes);
+  let view$ = Router
+    .map((output) => view(output));
+  return {
+    DOM: view$,
+    Router: route$
+  }
+}
+
 run(main, {
-  DOM: makeDOMDriver('#app')
+  DOM: makeDOMDriver('#app', {
+    'search': Search
+  }),
+  Router: makeRouterDriver({
+    html5history: false,
+    notFound: () => { return 'Page not found' }
+  })
 });
